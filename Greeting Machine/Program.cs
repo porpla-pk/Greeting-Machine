a72e05f45127b6ece425edc135ee284dc292954e
Porpla <Porpla@DESKTOP-H5OCL7P>
2024-06-19 12:55:13 -0400

    Add project files.

---
+++ Greeting Machine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greeting_Machine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask for User's name
+            Console.WriteLine("Hello! What's your name?");
+
+            // Capture User's Name
+            string name  = Console.ReadLine();
+
+            // Greet the user
+            Console.WriteLine("Nice to meet you " + name + ",how are you doing ?");
+
+            // Stop the program
+            Console.ReadKey();
+
+        }
+    }
+}
